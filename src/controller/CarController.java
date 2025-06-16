3864f1b9f454517b48bd7fb14734cd945f438493
NguyenDucAnh040924 <ducanh24092k4@gmail.com>
2025-06-16 10:09:33 +0700

    UPDATE

--- src/controller/CarController.java
+++ src/controller/CarController.java
@@ -4,6 +4,8 @@
  */
 package controller;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import model.CarType;
 import model.ColorType;
 import model.DaySellCar;
@@ -19,23 +21,41 @@ import view.ProgramView;
 public class CarController {
 
     private ProgramView view = new ProgramView();
-    private InputHandle inputHandle = new InputHandle();
 
     public void getView() {
         view.menuView();
     }
 
+    // get message for in/out put
+    private String getMessage(Locale currentLocale, String key, String path) {
+        ResourceBundle getValue = ResourceBundle.getBundle(path + currentLocale, currentLocale);
+        String value = getValue.getString(key);
+        return value + " ";
+    }
+
     public void run() {
         view.printTitle("Showroom car program");
         view.printView("%s", "Input information of car");
-        CarType name = CarType.AUDI;
+        CarType name = CarType.AUDI; // demo AUDI branch car
         ColorType color = null;
         PriceCar price = null;
         DaySellCar daySellCar = null;
-
-
     }
 
-
-
+    // FUNC: parse string to object
+    private CarType parseCarBranchString(String value){
+        return null;
+    }
+    
+    private ColorType parseColorTypeString(String value){
+        return null;
+    }
+    
+    private PriceCar parsePriceCarString(String value){
+        return null;
+    }
+    
+    private DaySellCar parseDaySellCarString(String value){
+        return null;
+    }
 }
---
+++ src/services/InputProcess.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import utils.InputHandle;
+
+/**
+ *
+ * @author NguyenDucAnh
+ */
+public class InputProcess {
+    
+    private InputHandle inputHandle = new InputHandle();
+    
+}
